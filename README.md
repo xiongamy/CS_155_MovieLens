9cf1ec300f077ff4203f5df6c699e36235bc0a63
Hippopotas <magichatman2@gmail.com>
2018-02-23 20:56:04 -0800

    Fixed graphlab to have all movies

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CS_155_MovieLens
 
-Please note that the graphlab code is written for Python 2.7, since graphlab is only compatible with Python 2.7.
+- Please note that the graphlab code is written for Python 2.7, since graphlab is only compatible with Python 2.7.
 
-In addition, the error measures in the results folder are mean squared error.
+- In addition, the error measures in the results folder are mean squared error.
--- src/graphlab_isnt_so_bad_after_all.py
+++ src/graphlab_isnt_so_bad_after_all.py
@@ -12,8 +12,8 @@ movie_cats = ['movie_id', 'movie_title', 'unknown', 'action', 'adventure',
 			  'romance', 'scifi', 'thriller', 'war', 'western']
 movies = graphlab.SFrame(pd.read_csv('../data/movies.txt', sep='\t', names=movie_cats, encoding='latin-1'))
 
-sim_model = graphlab.factorization_recommender.create(train, user_id='user_id', item_id='movie_id', target='rating',
-														num_factors=20, regularization=0.1, max_iterations=4000)
+sim_model = graphlab.factorization_recommender.create(train, user_id='user_id', item_id='movie_id', target='rating', item_data=movies,
+														num_factors=20, regularization=0.1, max_iterations=1000)
 
 print "\n"
 print "Test set info:"
