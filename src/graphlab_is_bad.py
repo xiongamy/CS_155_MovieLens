dadacfec6d9e38e2ffabc50a0c739f5413c33aab
Hippopotas <magichatman2@gmail.com>
2018-02-23 19:32:25 -0800

    ~V for graphlab

--- src/graphlab_is_bad.py
+++ src/graphlab_is_bad.py
@@ -1,4 +1,5 @@
 # Necessary to be in 2.7 to use graphlab.
+import numpy as np
 import pandas as pd
 import graphlab
 
@@ -12,12 +13,18 @@ movie_cats = ['movie_id', 'movie_title', 'unknown', 'action', 'adventure',
 movies = graphlab.SFrame(pd.read_csv('../data/movies.txt', sep='\t', names=movie_cats, encoding='latin-1'))
 
 sim_model = graphlab.factorization_recommender.create(train, user_id='user_id', item_id='movie_id', target='rating',
-														num_factors=20, regularization=0.1, max_iterations=1)
+														num_factors=20, regularization=0.1, max_iterations=4000)
 
+print "\n"
 print "Test set info:"
 rec = sim_model.evaluate(test)
 
 coef = sim_model.get('coefficients').get('movie_id').select_column('factors').to_numpy()
+
+R, Sigma, S = np.linalg.svd(np.transpose(coef))
+tildeV = np.matmul(coef, R[:, :2])
+
+np.savetxt('../results/v_graphlab.csv', tildeV, delimiter=',')
 
 #view = sim_model.views.overview(validation_set=test, item_data=movies, item_name_column='movie_title')
 #view.show()
